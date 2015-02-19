13bc393bb0352b46c2fc2cd0ba2eac67ea69091d
Scrubu <kylelai2016@gmail.com>
2015-02-19 18:23:08 -0500

    i get what to do

--- 02knightsTour/KnightsTour.java
+++ 02knightsTour/KnightsTour.java
@@ -14,7 +14,9 @@ public class KnightsTour{
     //instance variable
     private int[][]board;
 
-
+    public String name(){
+	return "lai.kyle";
+    }
     //toString()terminal specific character to move the cursor
     private String go(int x,int y){
 	return ("\033[" + x + ";" + y + "H");
@@ -30,8 +32,14 @@ public class KnightsTour{
 
     public String toString(){
 	String ans = "\n";
-	//build your knights tour here...
-	return hide + go(0,0) + ans + "\n" + show;
+	for(int x=0; x < board.length; x++){
+	    for(int y=0; y < board[x].length; y++){
+		ans+=board[x][y]+" ";
+	    }
+	    ans+="\n";
+	}
+	return  ans;
+	//	return hide + go(0,0) + ans + "\n" + show;
     }
 
     public KnightsTour(int size){
@@ -41,15 +49,18 @@ public class KnightsTour{
     
 
     public void solve(){
-	if(solve(0,0,1)){
-	    toString();
-	}			
+	if(!solve(0,0,1)){
+	    System.out.println("no solution");
+	}
+	else{
+	    System.out.println("solution works");
+	}
     }
 
 
     public void solve(int startx, int starty){
-	if(solve(startx,starty,1)){
-		toString();
+	if(!solve(startx,starty,1)){
+		 System.out.println("no solution");
 	    }
       	
     }
@@ -61,15 +72,23 @@ public class KnightsTour{
 	try{
 	boolean numb=false;
 	System.out.println(this);
-	wait(20);
+	wait(1);
 	if(x >= board[0].length || y >= board.length || board[x][y]!=0){
 	    return false;
 	}
-	board[x][y]=currentMoveNumber;
-	if(solve(x+2,y+1, currentMoveNumber+1)||solve(x+2,y-1,currentMoveNumber+1)||solve(x-2,y+1,currentMoveNumber+1)||solve(x-2,y-1,currentMoveNumber+1)){
+	 board[x][y]=currentMoveNumber;
+	if(solve(x+2,y+1, currentMoveNumber+1)
+	   ||solve(x+2,y-1,currentMoveNumber+1)
+	   ||solve(x-2,y+1,currentMoveNumber+1)
+	   ||solve(x-2,y-1,currentMoveNumber+1)
+	   ||solve(x-1,y-2,currentMoveNumber+1)
+	   ||solve(x-1,y+2,currentMoveNumber+1)
+	   ||solve(x+1,y-2,currentMoveNumber+1)
+	   ||solve(x+1,y+2,currentMoveNumber+1)){
 	    return true;
 	}
 	else{
+	    board[x][y]=0;
 	    return false;
 	}
 	}catch(ArrayIndexOutOfBoundsException n){
@@ -77,4 +96,8 @@ public class KnightsTour{
 	}
     }
 
+    public static void main(String[]args){
+		KnightsTour a = new KnightsTour(5);
+		a.solve();
+    }
 }
