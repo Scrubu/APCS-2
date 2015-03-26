e49ca67087d31c69d6ba11851a8f607d19f06f7c
Scrubu <kylelai2016@gmail.com>
2015-03-25 21:57:31 -0400

    finished working on resizing bug

---
+++ 10Deque/MyDeque.java
@@ -0,0 +1,104 @@
+import java.util.*;
+
+public class MyDeque<T>{
+
+    private Object[]ary;
+    private int size,head,tail;
+   
+    public MyDeque(){
+	ary = new Object[5];
+	size = 0;
+	head = 1;
+	tail = 0;
+    }
+    public int getS(){
+	return size;
+    }
+    public int getSize(){
+	return ary.length;
+    }
+    public void getHead(){
+	System.out.println(head);
+    }
+    public void getTail(){
+	System.out.println(tail);
+    }
+
+    public void resize(){
+	Object[] replace = new Object[ary.length*2];
+	int current = head;
+	int index = 0;
+	while(current != tail){
+	    if(current == ary.length){
+		current = 0;
+	    }
+	    if(current != tail){
+		replace[index]=ary[current];
+		index++;
+		current++;
+	    }
+	}
+	replace[index]=ary[tail];
+	tail=index;
+	head=0;
+	ary = replace;
+    }
+    public void addFirst(T value){
+	if(size == ary.length){
+	    resize();
+	}
+	if(head-1 < 0){
+	    head = ary.length-1;
+	}
+	else{
+	    head--;
+	}
+	ary[head]=value;
+	size++;
+    }
+    
+    public void addLast(T value){
+	if(size == ary.length){
+	    resize();
+	}
+	if(tail+1== ary.length){
+	    tail = 0;
+	}
+	else{
+	    tail++;
+	}
+	ary[tail]=value;
+	size++;
+	
+    }
+
+    public void removeFirst(){
+	ary[head]=null;
+	size--;
+	if(head+1 == ary.length){
+	    head = 0;
+	}
+	else{
+	    head++;
+	}
+    }
+
+    public String toString(){
+	String ans="";
+	for(int i = 0; i < ary.length; i++){
+	    ans+= " "+ary[i];
+	}
+	return ans;
+    }    
+    public static void main(String[] args){
+	MyDeque<String> MDQ = new MyDeque<String>();
+	MDQ.addLast("Oranges");
+	MDQ.addLast("APO");
+	MDQ.addLast("pun");
+	MDQ.addLast("shin");
+	MDQ.addFirst("johns");
+	MDQ.addLast("sum");
+	MDQ.addLast("nitsss");
+	System.out.println(MDQ);
+    }
+}
